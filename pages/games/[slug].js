85739f026265ac6b4bb089157f09c0ea4bf378fd
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-03 16:48:10 +0200

    feat: setup tailwind and basic page for games

--- pages/games/[slug].js
+++ pages/games/[slug].js
@@ -1,5 +1,6 @@
 import hydrate from 'next-mdx-remote/hydrate'
 import Head from 'next/head'
+import Image from 'next/image'
 
 import * as Util from '../../lib/util'
 
@@ -14,9 +15,33 @@ export default function Game({ source, frontMatter }) {
         <title>GBL@UZH - {frontMatter.title}</title>
       </Head>
 
-      <main>
+      <main className="p-4">
         <h1>{frontMatter.title}</h1>
+        <h2>{frontMatter.subtitle}</h2>
+        <Image
+          src={frontMatter.thumbnail}
+          height="200px"
+          width="300px"
+          layout="intrinsic"
+        />
+        <ul>
+          {frontMatter.objectives.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+        <ul>
+          {frontMatter.keywords.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
+        <ul>
+          {frontMatter.language.map((item) => (
+            <li>{item}</li>
+          ))}
+        </ul>
         {content}
+        {frontMatter.imprint}
+        {frontMatter.contact}
       </main>
     </div>
   )
