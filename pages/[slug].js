7cd1c09f0fd6f70535be618759e59f1a3bbe4ddb
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-03 15:02:00 +0200

    feat: dynamically create pages based on contents of the content/pages dir

---
+++ pages/[slug].js
@@ -0,0 +1,50 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import renderToString from 'next-mdx-remote/render-to-string'
+import hydrate from 'next-mdx-remote/hydrate'
+import Head from 'next/head'
+
+const components = {}
+
+export default function Page({ source, frontMatter }) {
+  const content = hydrate(source, { components })
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+      </Head>
+
+      <main>
+        <h1>{frontMatter.title}</h1>
+        {content}
+      </main>
+    </div>
+  )
+}
+
+export async function getStaticProps({ params }) {
+  const mdxPath = path.resolve(`content/pages/${params.slug}.mdx`)
+  const source = fs.readFileSync(mdxPath)
+  const { content, data } = matter(source)
+  const mdxSource = await renderToString(content, {
+    components,
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  })
+  return { props: { source: mdxSource, frontMatter: data } }
+}
+
+export async function getStaticPaths() {
+  const paths = fs
+    .readdirSync('content/pages/')
+    .filter((path) => /\.mdx?$/.test(path))
+    .map((path) => path.replace(/\.mdx?$/, ''))
+    .map((slug) => ({ params: { slug } }))
+
+  return { paths, fallback: false }
+}
--- pages/index.js
+++ pages/index.js
@@ -1,42 +1,12 @@
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
-import renderToString from 'next-mdx-remote/render-to-string'
-import hydrate from 'next-mdx-remote/hydrate'
 import Head from 'next/head'
 
-const components = {}
-
-export default function Home({ source, frontMatter }) {
-  const content = hydrate(source, { components })
-
+export default function Home() {
   return (
     <div>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
-
-      <main>
-        <h1>{frontMatter.title}</h1>
-        {content}
-      </main>
     </div>
   )
-}
-
-export async function getStaticProps() {
-  const mdxPath = path.resolve('content/pages/home.mdx')
-  const source = fs.readFileSync(mdxPath)
-  const { content, data } = matter(source)
-  const mdxSource = await renderToString(content, {
-    components,
-    mdxOptions: {
-      remarkPlugins: [],
-      rehypePlugins: [],
-    },
-    scope: data,
-  })
-  return { props: { source: mdxSource, frontMatter: data } }
 }
