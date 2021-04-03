19076d7d049147109182ed12d1d463140327d2d7
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-03 16:52:46 +0200

    chore: move next sources into src/

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>GBL@UZH</title>
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
       </Head>
     </div>
