d7ca659ceb5a86547241e029133895bf7c0f17dd
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-03 12:39:46 +0200

    basic next with mdx

--- next.config.js
+++ next.config.js
@@ -2,4 +2,7 @@ module.exports = {
   future: {
     webpack5: true,
   },
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    return config
+  },
 }
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,42 @@
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import renderToString from 'next-mdx-remote/render-to-string'
+import hydrate from 'next-mdx-remote/hydrate'
 import Head from 'next/head'
 
-export default function Home() {
+const components = {}
+
+export default function Home({ source, frontMatter }) {
+  const content = hydrate(source, { components })
+
   return (
     <div>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
-        <script>
-          {`
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            });
-          }
-        `}
-        </script>
       </Head>
 
-      <main>...</main>
+      <main>
+        <h1>{frontMatter.title}</h1>
+        {content}
+      </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const mdxPath = path.resolve('content/pages/home.mdx')
+  const source = fs.readFileSync(mdxPath)
+  const { content, data } = matter(source)
+  const mdxSource = await renderToString(content, {
+    components,
+    mdxOptions: {
+      remarkPlugins: [],
+      rehypePlugins: [],
+    },
+    scope: data,
+  })
+  return { props: { source: mdxSource, frontMatter: data } }
 }
