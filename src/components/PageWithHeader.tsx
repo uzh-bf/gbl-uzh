d960a7b55ca4e6542d4c266ebef9ecfc33b05c24
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-24 17:17:54 +0200

    fix: the max-width should not apply to the footer

--- src/components/PageWithHeader.tsx
+++ src/components/PageWithHeader.tsx
@@ -3,8 +3,8 @@ import Header from './Header'
 
 function PageWithHeader({ children }) {
   return (
-    <div className="flex flex-col h-full max-w-6xl m-auto">
-      <div className="flex-1 p-4">
+    <div className="flex flex-col h-full ">
+      <div className="flex-1 max-w-6xl p-4 m-auto">
         <Header />
         <div className="py-4 md:py-8">{children}</div>
       </div>
