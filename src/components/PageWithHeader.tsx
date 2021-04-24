cc0d42cfc93b5de3afd486efd55632a201b53b06
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-24 17:17:19 +0200

    feat: restrict the maximum width of the page

--- src/components/PageWithHeader.tsx
+++ src/components/PageWithHeader.tsx
@@ -3,7 +3,7 @@ import Header from './Header'
 
 function PageWithHeader({ children }) {
   return (
-    <div className="flex flex-col h-full">
+    <div className="flex flex-col h-full max-w-6xl m-auto">
       <div className="flex-1 p-4">
         <Header />
         <div className="py-4 md:py-8">{children}</div>
