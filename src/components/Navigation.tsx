a1157c2c8db66b6c76da1f8d4b697a141c9846cc
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-24 17:03:46 +0200

    feat: add initial links to navigation items

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,9 @@
-function NavigationItem({ children }): ReactElement {
+import Link from 'next/link'
+
+function NavigationItem({ children, href }): ReactElement {
   return (
     <div className="flex-1 p-1 mb-1 text-center shadow md:flex-initial md:mb-0 md:mr-2 md:p-2 md:last:mr-0 last:mb-0">
-      {children}
+      <Link href={href}>{children}</Link>
     </div>
   )
 }
@@ -9,12 +11,12 @@ function NavigationItem({ children }): ReactElement {
 function Navigation() {
   return (
     <div className="flex flex-col md:flex-row">
-      <NavigationItem>Knowledge Base</NavigationItem>
-      <NavigationItem>GBL @ IBF</NavigationItem>
-      <NavigationItem>Development Workflow</NavigationItem>
-      <NavigationItem>Roadmap</NavigationItem>
-      <NavigationItem>Resources</NavigationItem>
-      <NavigationItem>About Us</NavigationItem>
+      <NavigationItem href="kb">Knowledge Base</NavigationItem>
+      <NavigationItem href="dbf">GBL @ IBF</NavigationItem>
+      <NavigationItem href="dev">Development Workflow</NavigationItem>
+      <NavigationItem href="roadmap">Roadmap</NavigationItem>
+      <NavigationItem href="resources">Resources</NavigationItem>
+      <NavigationItem href="about">About Us</NavigationItem>
     </div>
   )
 }
