6fa5d1846546ac34a75def14f58b8d82783c002e
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-26 22:24:25 +0200

    feat: add link to hypernotes to the KB page

--- src/pages/kb.tsx
+++ src/pages/kb.tsx
@@ -1,3 +1,4 @@
+import Button from '../components/Button'
 import PageWithHeader from '../components/PageWithHeader'
 
 function KnowledgeBase() {
@@ -6,10 +7,20 @@ function KnowledgeBase() {
       <h1 className="mb-1 text-xl md:text-3xl md:mb-2">Knowledge Base</h1>
 
       <img
-        className="w-auto h-[500px] border"
+        className="w-auto h-[500px] border mb-2 md:mb-4"
         alt="Knowledge Graph"
         src="/images/knowledge_base.png"
       />
+
+      <Button
+        onClick={() =>
+          window.location.replace(
+            'https://hypernotes.zenkit.com/c/5bSlQ32jUN/notes?v=trbPByPXAP'
+          )
+        }
+      >
+        Open Knowledge Base
+      </Button>
     </PageWithHeader>
   )
 }
