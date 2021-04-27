c69f7260ca9c2302c75dfbfb91630fbda7e84951
Roland Schläfli <rolandschlaefli@gmail.com>
2021-04-27 14:03:34 +0200

    feat: base content structure for about and resources

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,16 @@
+import Header from '../components/common/Header'
 import PageWithHeader from '../components/PageWithHeader'
 
 function About() {
-  return <PageWithHeader title="About Us">...</PageWithHeader>
+  return (
+    <PageWithHeader title="About Us">
+      <Header.H1>About Us</Header.H1>
+      <Header.H2>Projects</Header.H2>
+      ...
+      <Header.H2>Project Team</Header.H2>
+      ...
+    </PageWithHeader>
+  )
 }
 
 export default About
--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,7 +1,18 @@
+import Header from '../components/common/Header'
 import PageWithHeader from '../components/PageWithHeader'
 
 function Resources() {
-  return <PageWithHeader title="Resources">...</PageWithHeader>
+  return (
+    <PageWithHeader title="Resources">
+      <Header.H1>Resources</Header.H1>
+      <Header.H2>Videos</Header.H2>
+      <Header.H2>Workshop Materials</Header.H2>
+      <Header.H2>Development Toolbox</Header.H2>
+      <Header.H3>Simulation Platform</Header.H3>
+      <Header.H3>Development Tooling</Header.H3>
+      <Header.H2>Literature</Header.H2>
+    </PageWithHeader>
+  )
 }
 
 export default Resources
