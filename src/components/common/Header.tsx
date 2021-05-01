ba709e1cea184ad69681163810107e65703fcb85
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-01 14:59:40 +0200

    feat: use uzh blue for headers

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -11,7 +11,12 @@ const defaultProps = {
 
 function H1({ children, className }: Props) {
   return (
-    <h1 className={clsx('mb-2 text-xl md:text-2xl md:mb-4', className)}>
+    <h1
+      className={clsx(
+        'mb-2 text-xl md:text-2xl md:mb-4 text-uzh-blue-60 font-bold',
+        className
+      )}
+    >
       {children}
     </h1>
   )
@@ -19,7 +24,12 @@ function H1({ children, className }: Props) {
 
 function H2({ children, className }: Props) {
   return (
-    <h2 className={clsx('mb-2 text-lg md:text-xl md:mb-4', className)}>
+    <h2
+      className={clsx(
+        'mb-2 text-lg md:text-xl md:mb-4 text-uzh-blue-60 font-bold',
+        className
+      )}
+    >
       {children}
     </h2>
   )
@@ -27,7 +37,12 @@ function H2({ children, className }: Props) {
 
 function H3({ children, className }: Props) {
   return (
-    <h3 className={clsx('mb-2 text-base md:text-lg md:mb-4', className)}>
+    <h3
+      className={clsx(
+        'mb-2 text-base md:text-lg md:mb-4 text-uzh-blue-60 font-bold',
+        className
+      )}
+    >
       {children}
     </h3>
   )
