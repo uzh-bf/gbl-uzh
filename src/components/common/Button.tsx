220bc2dfdb7009862e3b9560fef66ab1e6820966
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-01 14:58:35 +0200

    feat: use uzh blue and bold font for buttons

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -10,7 +10,7 @@ function Button({ children, onClick }: Props) {
     <button
       onClick={onClick}
       type="button"
-      className="inline-flex items-center px-4 py-2 text-sm text-gray-700 bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50"
+      className="inline-flex items-center px-4 py-2 text-sm font-bold bg-white border border-gray-300 rounded-md shadow-sm text-uzh-blue-60 hover:bg-gray-50"
     >
       {children}
     </button>
