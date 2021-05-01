6b48edac786a3312d7a2caf4053591e38ce8aab1
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-01 14:44:17 +0200

    feat: extend home section with icon content subcomponent

--- src/components/common/HomeSection.tsx
+++ src/components/common/HomeSection.tsx
@@ -6,6 +6,7 @@ interface Props {
   children: React.ReactNode
   className?: string
 }
+
 function HomeSection({ title, children, className }: Props) {
   return (
     <PaddedSection className={className}>
@@ -19,6 +20,27 @@ function HomeSection({ title, children, className }: Props) {
 
 HomeSection.defaultProps = {
   className: undefined,
+}
+
+interface IconContentProps {
+  iconSrc: string
+  iconAlt: string
+  children: React.ReactNode
+}
+
+HomeSection.IconContent = function IconContent({
+  iconSrc,
+  iconAlt,
+  children,
+}: IconContentProps) {
+  return (
+    <div className="relative flex flex-col items-center mb-2 md:flex-row md:mb-4">
+      <div className="flex items-center justify-center w-full md:w-52">
+        <img src={iconSrc} alt={iconAlt} />
+      </div>
+      <p className="p-2 prose md:p-4 md:pl-16">{children}</p>
+    </div>
+  )
 }
 
 export default HomeSection
