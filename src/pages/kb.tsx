ae434a049a01515bb057193821a56845219b09e6
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-01 12:32:03 +0200

    feat: embed the knowledge base

--- src/pages/kb.tsx
+++ src/pages/kb.tsx
@@ -1,26 +1,14 @@
-import Button from '../components/common/Button'
 import PageWithHeader from '../components/PageWithHeader'
 
 function KnowledgeBase() {
   return (
     <PageWithHeader title="Knowledge Base">
-      <h1 className="mb-1 text-xl md:text-3xl md:mb-2">Knowledge Base</h1>
-
-      <img
-        className="w-auto h-[500px] border mb-2 md:mb-4"
-        alt="Knowledge Graph"
-        src="/images/knowledge_base.png"
+      <iframe
+        src="https://www.gbl.uzh.ch/md/"
+        title="Game-Based Learning"
+        height="100%"
+        width="100%"
       />
-
-      <Button
-        onClick={() =>
-          window.location.replace(
-            'https://hypernotes.zenkit.com/c/5bSlQ32jUN/notes?v=trbPByPXAP'
-          )
-        }
-      >
-        Open Knowledge Base
-      </Button>
     </PageWithHeader>
   )
 }
