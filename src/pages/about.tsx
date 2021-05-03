7cc5ab7e8c32bd76f5b0b53753e88af4433e1a86
Roland Schläfli <rolandschlaefli@gmail.com>
2021-05-03 16:50:48 +0200

    feat: basic structure for resources

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import Header from '../components/common/Header'
+import Title from '../components/common/Title'
+import TitleBackground from '../components/common/TitleBackground'
 import PageWithHeader from '../components/PageWithHeader'
 
 function About() {
@@ -30,23 +32,28 @@ function About() {
 
   return (
     <PageWithHeader title="About Us">
-      <Header.H1>About Us</Header.H1>
-      <Header.H2>Projects</Header.H2>
-      ...
-      <Header.H2>Project Team</Header.H2>
-      ...
-      <Header.H2>Get In Touch</Header.H2>
-      <p className="mb-4 prose max-w-none">
-        If you would like to contact our project team, please fill out the
-        following form and we will gladly get in touch.
-      </p>
-      <div className="max-w-lg border">
-        <div id="c7">
-          Fill in the{' '}
-          <a href="https://www.bf-tools.uzh.ch/applications/easyforms/index.php?r=app%2Fform&id=13">
-            online form
-          </a>
-          .
+      <TitleBackground>
+        <Title title="About Us" />
+      </TitleBackground>
+
+      <div className="max-w-6xl p-4 m-auto">
+        <Header.H2>Projects</Header.H2>
+        ...
+        <Header.H2>Project Team</Header.H2>
+        ...
+        <Header.H2>Get In Touch</Header.H2>
+        <p className="mb-4 prose max-w-none">
+          If you would like to contact our project team, please fill out the
+          following form and we will gladly get in touch.
+        </p>
+        <div className="max-w-lg border">
+          <div id="c7">
+            Fill in the{' '}
+            <a href="https://www.bf-tools.uzh.ch/applications/easyforms/index.php?r=app%2Fform&id=13">
+              online form
+            </a>
+            .
+          </div>
         </div>
       </div>
     </PageWithHeader>
--- src/pages/resources.tsx
+++ src/pages/resources.tsx
@@ -1,16 +1,23 @@
 import Header from '../components/common/Header'
+import Title from '../components/common/Title'
+import TitleBackground from '../components/common/TitleBackground'
 import PageWithHeader from '../components/PageWithHeader'
 
 function Resources() {
   return (
     <PageWithHeader title="Resources">
-      <Header.H1>Resources</Header.H1>
-      <Header.H2>Videos</Header.H2>
-      <Header.H2>Workshop Materials</Header.H2>
-      <Header.H2>Development Toolbox</Header.H2>
-      <Header.H3>Simulation Platform</Header.H3>
-      <Header.H3>Development Tooling</Header.H3>
-      <Header.H2>Literature</Header.H2>
+      <TitleBackground>
+        <Title title="Resources" />
+      </TitleBackground>
+
+      <div className="max-w-6xl p-4 m-auto">
+        <Header.H2>Videos</Header.H2>
+        <Header.H2>Workshop Materials</Header.H2>
+        <Header.H2>Development Toolbox</Header.H2>
+        <Header.H3>Simulation Platform</Header.H3>
+        <Header.H3>Development Tooling</Header.H3>
+        <Header.H2>Literature</Header.H2>
+      </div>
     </PageWithHeader>
   )
 }
