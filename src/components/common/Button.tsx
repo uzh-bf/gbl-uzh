8618a9182fb39f2ce807d274032db01685f13fa9
Roland Schläfli <rolandschlaefli@gmail.com>
2021-06-15 22:21:09 +0200

    feat: contents for the dev page

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -2,19 +2,22 @@ import { ArrowRightIcon } from '@heroicons/react/solid'
 import clsx from 'clsx'
 
 interface Props {
+  disabled?: boolean
   children: React.ReactNode
   className?: string
-  onClick: () => void
+  onClick?: () => void
 }
 
-function Button({ children, onClick, className }: Props) {
+function Button({ disabled, children, onClick, className }: Props) {
   return (
     <button
+      disabled={disabled}
       onClick={onClick}
       type="button"
       className={clsx(
-        'inline-flex items-center px-4 py-2 text-sm font-bold bg-white border border-gray-300 rounded-md shadow-sm hover:bg-gray-50',
-        className
+        'inline-flex items-center px-4 py-2 text-sm font-bold bg-white border border-gray-300 rounded-md shadow-sm ',
+        className,
+        disabled ? 'bg-gray-200 cursor-default' : 'hover:bg-gray-50'
       )}
     >
       {children}
@@ -23,7 +26,9 @@ function Button({ children, onClick, className }: Props) {
 }
 
 Button.defaultProps = {
+  disabled: false,
   className: '',
+  onClick: undefined,
 }
 
 Button.Arrow = function ButtonArrow() {
--- src/pages/dev.tsx
+++ src/pages/dev.tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import { useRouter } from 'next/router'
+import WorkflowImage from '../../public/images/dev_workflow.png'
 import Button from '../components/common/Button'
 import Header from '../components/common/Header'
 import Title from '../components/common/Title'
@@ -15,7 +17,9 @@ function DevelopmentWorkflow() {
         <Title title="Game Development" />
       </TitleBackground>
       <Content>
-        <Header.H2>Introduction</Header.H2>
+        <Header.H2 className="mt-4">
+          Principles of Game-Based Learning
+        </Header.H2>
         <p className="prose max-w-none">
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
           nostrum praesentium vitae reprehenderit incidunt libero. Fugit harum,
@@ -27,29 +31,54 @@ function DevelopmentWorkflow() {
           Details
         </Button>
 
-        <Header.H2 className="mt-16">Game Topic</Header.H2>
-        <p className="prose max-w-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          nostrum praesentium vitae reprehenderit incidunt libero. Fugit harum,
-          aut repellendus impedit tempore cum iure, alias ipsam earum facere
-          praesentium doloribus deserunt.
-        </p>
+        <Header.H2 className="mt-16">Game Development Workflow</Header.H2>
+        <Image src={WorkflowImage} alt="Game Development Workflow" />
 
-        <Header.H2 className="mt-16">Game Development</Header.H2>
+        <Header.H3 className="mt-8">Game Topic</Header.H3>
         <p className="prose max-w-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          nostrum praesentium vitae reprehenderit incidunt libero. Fugit harum,
-          aut repellendus impedit tempore cum iure, alias ipsam earum facere
-          praesentium doloribus deserunt.
+          Before starting with the actual game development, it is necessary to
+          evaluate which topics are suitable and useful for an implementation in
+          the form of game-based learning. Game-based learning is a form of
+          teaching and learning that can be particularly motivating and
+          sustainable. However, it is recommended to conduct a cost-benefit
+          analysis before making the actual development decision. The
+          implementation of complex content in a playable form requires a
+          considerable amount of resources in terms of content and technology.
         </p>
+        <Button disabled className="mt-4">
+          <Button.Arrow />
+          Coming Soon
+        </Button>
 
-        <Header.H2 className="mt-16">Game Execution</Header.H2>
+        <Header.H3 className="mt-8">Game Development</Header.H3>
         <p className="prose max-w-none">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis
-          nostrum praesentium vitae reprehenderit incidunt libero. Fugit harum,
-          aut repellendus impedit tempore cum iure, alias ipsam earum facere
-          praesentium doloribus deserunt.
+          After the decision to implement a particular game idea, the actual
+          game development takes place. A loop-like approach involving the
+          content and technical developers is recommended. Individual game
+          elements should be tested and evaluated on an ongoing basis so that
+          corrective measures can be taken promptly if necessary.
         </p>
+        <Button disabled className="mt-4">
+          <Button.Arrow />
+          Coming Soon
+        </Button>
+
+        <Header.H3 className="mt-8">Game Execution</Header.H3>
+        <p className="prose max-w-none">
+          When a game (part) is ready to play, it should be implemented in a
+          suitable learning setting. Necessary theories and teaching materials
+          should be taught at appropriate points before, during or after the
+          game itself. Whether in the virtual or physical classroom - a
+          didactically sound embedding in the flow of the lesson is important
+          for a sustainable learning success of the participants. In order to
+          find a suitable game setting and to further develop the game (part),
+          it is helpful to always ask for feedback and to regularly evaluate the
+          game and the game setting.
+        </p>
+        <Button disabled className="mt-4">
+          <Button.Arrow />
+          Coming Soon
+        </Button>
       </Content>
     </PageWithHeader>
   )
