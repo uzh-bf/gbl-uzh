3c79e2eba99c32f1a8d53ea0e2f2793276a136b5
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-07-26 16:01:08 +0200

    feat: setup analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,22 @@
 import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 
 function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   // eslint-disable-next-line react/jsx-props-no-spreading
   return <Component {...pageProps} />
 }
