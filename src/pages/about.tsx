08328cb48578bdd01b63d3a535561d9a920ccf22
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-07-27 17:16:35 +0200

    feat: initial draft for project overview

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,6 +1,4 @@
-import Image from 'next/image'
 import { useEffect } from 'react'
-import ProjectImage from '../../public/images/projects.png'
 import RolandImage from '../../public/images/schlaefli_roland.jpg'
 import DavidImage from '../../public/images/schmocker_david.jpg'
 import BenjaminImage from '../../public/images/wilding_benjamin.jpg'
@@ -44,7 +42,69 @@ function About() {
       </TitleBackground>
 
       <Content>
-        <Image src={ProjectImage} alt="Projects" placeholder="blur" />
+        <Header.H2>Project Overview</Header.H2>
+        <div className="flex flex-col sm:flex-row">
+          <div className="flex-1">
+            <div className="p-2 font-bold text-center text-white bg-uzh-blue-80">
+              Project 1
+            </div>
+            <div className="h-full p-2 prose bg-uzh-gray-20">
+              <p>
+                <span className="font-bold">Project Duration:</span> 2019-2021
+                <br />
+                Supported by Swissuniversities and the University of Zurich
+              </p>
+              <div className="font-bold">Project Goals</div>
+              <ul>
+                <li>
+                  Conduct workshops for lecturers and students supported by
+                  experts.
+                </li>
+                <li>
+                  Implement the results of workshops and theses, in which
+                  lecturers and students created their own simulations.
+                </li>
+                <li>
+                  Develop initial guidelines for the efficient implementation of
+                  simulations.
+                </li>
+                <li>
+                  Incorporate new simulations in the regular degree programs.
+                </li>
+              </ul>
+            </div>
+          </div>
+
+          <div className="flex-1 mt-4 sm:ml-4 sm:mt-0">
+            <div className="p-2 font-bold text-center text-white bg-uzh-blue-80">
+              Project 2
+            </div>
+
+            <div className="h-full p-2 prose bg-uzh-gray-20">
+              <p>
+                <span className="font-bold">Project Duration:</span> 2021-2023
+                <br />
+                Supported by Swissuniversities and the University of Zurich
+              </p>
+              <div className="font-bold">Project Goals</div>
+              <ul>
+                <li>
+                  Support the creation process of digital learning games
+                  (especially serious games and simulations using newly created
+                  content-related, didactic and technical resources.
+                </li>
+                <li>
+                  Establish and maintain a Game-Based Learning community for
+                  higher education.
+                </li>
+                <li>
+                  Develop an overview of Game-Based Learning in practice at the
+                  University of Zurich.
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
 
         <Header.H2 className="mt-16">Project Team</Header.H2>
         <div>
