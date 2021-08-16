ecb7004761d2e19247c16a1050ecdf396d725504
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-16 15:10:29 +0200

    refactor: read game contents from kb files with correct title casing of names

--- website/src/lib/util.ts
+++ website/src/lib/util.ts
@@ -3,15 +3,24 @@ import matter from 'gray-matter'
 import { serialize } from 'next-mdx-remote/serialize'
 import path from 'path'
 
-export function getStaticProps(dir_name, components = {}) {
+export function getStaticProps(dir_name) {
   return async ({ params }) => {
+    // slugs come in like "portfolio-management-game"
+    // but we want to read from files like "Portfolio Management Game.md"
+    const filenameTitleCase = params.slug
+      .trim()
+      .replace(/-/g, ' ')
+      .toLowerCase()
+      // all independent words should begin with a capital character
+      .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()))
+
     const mdxPath = path.join(
       process.cwd(),
-      `content/${dir_name}/${params.slug}.mdx`
+      `../kb/${dir_name}/${filenameTitleCase}.md`
     )
     const source = fs.readFileSync(mdxPath)
     const { content, data } = matter(source)
-    const mdxSource = await serialize(content)
+    const mdxSource = await serialize(content, { scope: data })
     return { props: { source: mdxSource, frontMatter: data } }
   }
 }
@@ -19,9 +28,14 @@ export function getStaticProps(dir_name, components = {}) {
 export function getStaticPaths(dir_name) {
   return async () => {
     const paths = fs
-      .readdirSync(path.join(process.cwd(), `content/${dir_name}/`))
-      .filter((p) => /\.mdx?$/.test(p))
-      .map((p) => p.replace(/\.mdx?$/, ''))
+      .readdirSync(path.join(process.cwd(), `../kb/${dir_name}/`))
+      .filter((p) => /\.md?$/.test(p))
+      .map((p) =>
+        p
+          .replace(/\.md?$/, '')
+          .replace(/\s/g, '-')
+          .toLowerCase()
+      )
       .map((slug) => ({ params: { slug } }))
 
     return { paths, fallback: false }
--- website/src/pages/api/auth.ts
+++
@@ -1,3 +0,0 @@
-import { createVercelBeginHandler } from 'netlify-cms-oauth-provider-node'
-
-module.exports = createVercelBeginHandler({}, { useEnv: true })
