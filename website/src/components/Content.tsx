1a6754eb4c37f00fa3f383af3d4f1249558232eb
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-16 22:16:49 +0200

    style: reduce padding on mobile

--- website/src/components/Content.tsx
+++ website/src/components/Content.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 function Content({ children }: Props) {
-  return <div className="max-w-6xl p-8 m-auto">{children}</div>
+  return <div className="max-w-6xl p-4 m-auto md:p-8">{children}</div>
 }
 
 export default Content
