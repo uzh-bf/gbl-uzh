cc5fde76009e230cad9a0b4b1bcee2f088d0c11f
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-08-20 16:11:50 +0200

    style: add footer background and make logo images consistent

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -5,14 +5,20 @@ import UZHLogo from '../../public/images/logo_uzh.jpeg'
 
 function Footer() {
   return (
-    <footer className="z-10 mt-8 text-sm text-gray-600 bg-white border-t-2 border-uzh-red-100 border-top md:text-base">
+    <footer className="z-10 mt-8 text-sm text-gray-600 border-t-2 bg-uzh-gray-20 border-uzh-red-100 border-top md:text-base">
       <div className="flex flex-col justify-between flex-initial max-w-6xl px-4 py-16 m-auto md:px-8 md:flex-row">
         <div className="flex flex-col items-center order-1 mb-4 md:order-2 md:flex-row md:mb-0">
-          <div className="w-40 mb-3 md:mb-0 md:mr-8">
-            <Image src={UZHLogo} alt="UZH Logo" />
+          <div className="relative w-40 h-20 p-4 mb-3 bg-white border md:mb-0 md:mr-8">
+            <Image layout="responsive" src={UZHLogo} alt="UZH Logo" />
           </div>
-          <div className="w-40">
-            <Image src={SwissUniLogo} alt="Swissuniversities Logo" />
+          <div className="flex items-center w-40 h-20 p-4 bg-white border">
+            <div className="relative w-full">
+              <Image
+                layout="responsive"
+                src={SwissUniLogo}
+                alt="Swissuniversities Logo"
+              />
+            </div>
           </div>
         </div>
 
