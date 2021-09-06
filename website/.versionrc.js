3c5f5cfe8c15f543ffafe188bf3807c53e0e3020
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-09-06 18:24:56 +0200

    chore(release): 0.2.0

---
+++ website/.versionrc.js
@@ -0,0 +1,60 @@
+module.exports = {
+  types: [
+    {
+      type: 'feat',
+      section: 'Features',
+    },
+    {
+      type: 'enhance',
+      section: 'Enhancements',
+    },
+    {
+      type: 'fix',
+      section: 'Bug Fixes',
+    },
+    {
+      type: 'docs',
+      section: 'Documentation',
+    },
+    {
+      type: 'refactor',
+      section: 'Refactors',
+    },
+    {
+      type: 'perf',
+      section: 'Performance',
+    },
+    {
+      type: 'deploy',
+      section: 'Deployment',
+    },
+    {
+      type: 'deps',
+      section: 'Dependencies',
+    },
+    {
+      type: 'build',
+      section: 'Build and CI',
+    },
+    {
+      type: 'ci',
+      section: 'Build and CI',
+    },
+    {
+      type: 'chore',
+      section: 'Other',
+    },
+    {
+      type: 'wip',
+      section: 'Other',
+    },
+    {
+      type: 'test',
+      section: 'Other',
+    },
+    {
+      type: 'style',
+      section: 'Other',
+    },
+  ],
+}
