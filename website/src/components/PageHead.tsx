bab2e882b1c9465f0bb914ebfd52022b3a791c03
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-10-27 20:04:54 +0200

    enhance(website): replace logo with new working draft

--- website/src/components/PageHead.tsx
+++ website/src/components/PageHead.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import React, { useState } from 'react'
-import LogoImage from '../../public/images/logo_temp_beta_orange.png'
+import LogoImage from '../../public/images/GBLUZH.png'
 import customLoader from '../lib/loader'
 
 const NAVIGATION_ITEMS = [
@@ -114,7 +114,7 @@ function Logo() {
   return (
     <Link href="/" passHref>
       <a className="flex-1 md:pl-8">
-        <div className="relative w-full h-16 md:w-40 md:h-full">
+        <div className="relative w-full h-20 md:w-56 md:h-full">
           <Image
             loader={customLoader}
             src={LogoImage}
