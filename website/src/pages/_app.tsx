eb23dd06fdfd9d096dd5f6b915027141f22d10e9
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-10-28 15:44:45 +0200

    enhance(website): add openreplay integration

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -39,6 +39,28 @@ function App({ Component, pageProps }: AppProps) {
           `,
         }}
       />
+      <Script
+        id="openreplay-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(A,s,a,y,e,r){
+              r=window.OpenReplay=[s,r,e,[y-1]];
+              s=document.createElement('script');s.src=a;s.async=!A;
+              document.getElementsByTagName('head')[0].appendChild(s);
+              r.start=function(v){r.push([0])};
+              r.stop=function(v){r.push([1])};
+              r.setUserID=function(id){r.push([2,id])};
+              r.setUserAnonymousID=function(id){r.push([3,id])};
+              r.setMetadata=function(k,v){r.push([4,k,v])};
+              r.event=function(k,p,i){r.push([5,k,p,i])};
+              r.issue=function(k,p){r.push([6,k,p])};
+              r.isActive=function(){return false};
+              r.getSessionToken=function(){};
+            })(0, ${process.env.NEXT_PUBLIC_OPENREPLAY_PROJECT_KEY}, "//static.openreplay.com/latest/openreplay.js",1,93);
+          `,
+        }}
+      />
       <Component {...pageProps} />
     </>
   )
