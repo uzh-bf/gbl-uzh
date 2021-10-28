7be8761195134b33a600a904644121169a12e80c
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-10-28 15:27:14 +0200

    refactor(website): adjust gtag according to official example

--- website/src/pages/_app.tsx
+++ website/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { AppProps } from 'next/dist/shared/lib/router/router'
 import { useRouter } from 'next/router'
+import Script from 'next/script'
 import { useEffect } from 'react'
 import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
@@ -17,8 +18,30 @@ function App({ Component, pageProps }: AppProps) {
     }
   }, [router.events])
 
-  // eslint-disable-next-line react/jsx-props-no-spreading
-  return <Component {...pageProps} />
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default App
