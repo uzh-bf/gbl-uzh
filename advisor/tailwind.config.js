833c37c2dd4142fe38054569cffe3765b4819b65
Roland Schlaefli <rolandschlaefli@gmail.com>
2021-12-01 16:36:32 +0100

    feat(advisor): add advisor in separate directory

---
+++ advisor/tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  // purge: ['Gamification Advisor.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  corePlugins: {
+    preflight: false,
+  },
+}
