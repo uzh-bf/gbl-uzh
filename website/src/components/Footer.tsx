c95def64b2d6302a4e4c2d9151504ba3a35bd766
Roland Schlaefli <rolandschlaefli@gmail.com>
2022-04-06 14:39:50 +0200

    enhance(website): add links to uzh and swissuni in footer

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -9,26 +9,34 @@ function Footer() {
     <footer className="z-10 mt-8 text-sm text-gray-600 border-t-2 bg-uzh-gray-20 border-uzh-red-100 border-top md:text-base">
       <div className="flex flex-col justify-between flex-initial max-w-6xl px-4 py-16 m-auto md:px-8 md:flex-row">
         <div className="flex flex-col items-center order-1 mb-4 md:order-2 md:flex-row md:mb-0">
-          <div className="relative w-40 h-20 p-4 mb-3 bg-white border md:mb-0 md:mr-8">
-            <Image
-              placeholder="blur"
-              loader={customLoader}
-              layout="intrinsic"
-              src={UZHLogo}
-              alt="UZH Logo"
-            />
-          </div>
-          <div className="flex items-center w-40 h-20 p-4 bg-white border">
-            <div className="relative w-full">
+          <a href="https://www.uzh.ch" target="_blank" rel="noreferrer">
+            <div className="relative w-40 h-20 p-4 mb-3 bg-white border md:mb-0 md:mr-8">
               <Image
                 placeholder="blur"
                 loader={customLoader}
                 layout="intrinsic"
-                src={SwissUniLogo}
-                alt="Swissuniversities Logo"
+                src={UZHLogo}
+                alt="UZH Logo"
               />
             </div>
-          </div>
+          </a>
+          <a
+            href="https://www.swissuniversities.ch"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <div className="flex items-center w-40 h-20 p-4 bg-white border">
+              <div className="relative w-full">
+                <Image
+                  placeholder="blur"
+                  loader={customLoader}
+                  layout="intrinsic"
+                  src={SwissUniLogo}
+                  alt="Swissuniversities Logo"
+                />
+              </div>
+            </div>
+          </a>
         </div>
 
         <p className="order-2 prose-sm prose text-center text-gray-600 md:order-1 md:text-left">
