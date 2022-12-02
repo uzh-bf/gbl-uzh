dfaee86adcf46830317dfe9216279bc071f66aff
andrinr <andrinrehmann@gmail.com>
2022-12-02 17:46:54 +0100

    start action reducers

--- apps/demo-game/src/reducers/ActionsReducer.ts
+++ apps/demo-game/src/reducers/ActionsReducer.ts
@@ -1,10 +1,43 @@
+import { match } from 'ts-pattern'
+
 export enum ActionTypes {
   DECIDE_BANK = 'DECIDE_BANK',
   DECIDE_BONDS = 'DECIDE_BONDS',
   DECIDE_STOCK = 'DECIDE_STOCK',
 }
 
-export function apply(state: any, action: any) {
-  console.log('actions', state, action)
-  return state
+export function apply(state: any, action: Actions) {
+  console.log('segment', state, action)
+
+  return match(action)
+    .with({ type: ActionTypes.DECIDE_BANK }, () => {
+      const {
+        segmentIx,
+        segmentCount,
+        periodFacts,
+        previousSegmentFacts,
+        periodIx,
+      } = action.payload
+
+      //periodFacts.
+      return {
+        type: action.type,
+        result: state,
+      }
+    })
+    .with({ type: ActionTypes.DECIDE_BONDS }, () => {
+
+      return {
+        type: action.type,
+        result: state,
+      }
+    })
+    .with({ type: ActionTypes.DECIDE_BANK }, () => {
+      
+      return {
+        type: action.type,
+        result: state,
+      }
+    })
+    .exhaustive()
 }
