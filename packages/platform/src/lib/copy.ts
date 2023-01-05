89c5692f1a4b08d76090ec538a41360e884eb3b0
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-01-05 13:05:58 +0100

    fix(packages/platform): embed graphql documents in platform dist

--- packages/platform/src/lib/copy.ts
+++ packages/platform/src/lib/copy.ts
@@ -1,7 +1,12 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 const path = require('path')
 
-fs.copyFileSync(
+fs.copy(
   path.resolve(__dirname, '../../prisma/schema.prisma'),
   path.resolve(__dirname, '../../dist/schema.prisma')
+)
+
+fs.copy(
+  path.resolve(__dirname, '../../src/ops'),
+  path.resolve(__dirname, '../../dist/ops')
 )
--- packages/platform/tsup.config.ts
+++ packages/platform/tsup.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   entry: [
     'src/index.ts',
     'src/nexus.ts',
-    'src/generated/ops.ts',
+    // 'src/generated/ops.ts',
     'src/lib/util.ts',
     'src/lib/apollo.ts',
   ],
