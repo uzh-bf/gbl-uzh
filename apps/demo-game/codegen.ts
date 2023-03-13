a70c13aff14cb6667f4808300c12236b626982c1
Roland Schlaefli <rolandschlaefli@gmail.com>
2023-03-13 14:25:53 +0100

    deploy(apps/demo-game): fix docker build

--- apps/demo-game/codegen.ts
+++ apps/demo-game/codegen.ts
@@ -3,7 +3,10 @@ import type { CodegenConfig } from '@graphql-codegen/cli'
 const config: CodegenConfig = {
   overwrite: true,
   schema: 'src/graphql/generated/schema.graphql',
-  documents: '../../node_modules/@gbl-uzh/platform/dist/ops/**/*.graphql',
+  documents:
+    process.env.NODE_ENV === 'production'
+      ? 'node_modules/@gbl-uzh/platform/dist/ops/**/*.graphql'
+      : '../../node_modules/@gbl-uzh/platform/dist/ops/**/*.graphql',
   ignoreNoDocuments: true, // for better experience with the watcher
   generates: {
     'src/graphql/generated/ops.ts': {
