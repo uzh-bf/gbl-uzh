77723c99a483cf58b2944c83dd6209c3ae8e507a
jajakob <j.jacob.cano@gmail.com>
2024-06-04 11:34:47 +0200

    switch statement in the period reducer

--- apps/demo-game/src/reducers/PeriodReducer.ts
+++ apps/demo-game/src/reducers/PeriodReducer.ts
@@ -2,7 +2,6 @@ import { Action } from '@gbl-uzh/platform'
 import { debugLog } from '@gbl-uzh/platform/dist/lib/util'
 import { PeriodFacts, PeriodSegmentFacts } from '@graphql/index'
 import { PrismaClient } from '@prisma/client'
-import { match } from 'ts-pattern'
 
 export enum ActionTypes {
   PERIOD_INITIALIZE = 'PERIOD_INITIALIZE',
@@ -30,26 +29,32 @@ type Actions =
     >
 
 export function apply(state: any, action: Actions) {
-  const newState = match(action)
-    .with({ type: ActionTypes.PERIOD_INITIALIZE }, () => {
-      return {
-        type: action.type,
-        result: {
-          ...state,
-        },
-        isDirty: true,
-      }
-    })
-    .with({ type: ActionTypes.PERIOD_CONSOLIDATE }, () => {
-      return {
-        type: action.type,
-        result: state,
+  const output = {
+    type: action.type,
+    result: state,
+    isDirty: true,
+  }
+
+  let newState
+  switch (action.type) {
+    // TODO(Jakob):
+    // Is it ok if we add here events and notifications as well?
+    // -> it would be more consistent
+    case ActionTypes.PERIOD_INITIALIZE:
+      newState = output
+      break
+    case ActionTypes.PERIOD_CONSOLIDATE:
+      newState = {
+        ...output,
+        isDirty: false,
         events: [],
         notification: [],
-        isDirty: false,
       }
-    })
-    .exhaustive()
+      break
+
+    default:
+      break
+  }
 
   debugLog('PeriodReducer', state, action, newState)
 
