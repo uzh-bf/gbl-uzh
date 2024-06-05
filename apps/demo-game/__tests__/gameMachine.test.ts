e14119c9aa8ebcb53109beed42d87cab91e71bb7
jajakob <j.jacob.cano@gmail.com>
2024-06-05 12:26:38 +0200

    testing demo game, create or find game

--- apps/demo-game/__tests__/gameMachine.test.ts
+++ apps/demo-game/__tests__/gameMachine.test.ts
@@ -4,6 +4,9 @@ import { PrismaClient } from '@prisma/client'
 describe('Testing Demo Game', () => {
   const nameGame = 'TestDemoGame'
   const playerCount = 1
+  const createNewGame = true
+  let gameId = 1
+  let game
   const userSub = '716f7632-ed33-4701-a281-0f27bd4f6e82'
   const roleAssigner = (ix: number): UserRole => UserRole.PLAYER
   const prisma = new PrismaClient()
@@ -16,9 +19,46 @@ describe('Testing Demo Game', () => {
     },
   }
 
+  const findGame = async (gameId: number) => {
+    const results = {
+      include: {
+        player: true,
+      },
+    }
+
+    return prisma.game.findUnique({
+      where: {
+        id: gameId,
+      },
+      include: {
+        players: true,
+        periods: true,
+        activePeriod: {
+          include: {
+            results: results,
+            nextPeriod: true,
+            previousPeriod: {
+              include: {
+                results: results,
+              },
+            },
+            activeSegment: {
+              include: {
+                results: results,
+              },
+            },
+            decisions: {
+              include: {
+                player: true,
+              },
+            },
+          },
+        },
+      },
+    })
+  }
+
   // TODO(Jakob):
-  // - flag if it should create new game or find existing one
-  //   (implement helper for that)
   // - flag for clearing the prisma db and do from scratch
   // - what is the return of GameService.createGame and what is it used for?
 
@@ -26,10 +66,17 @@ describe('Testing Demo Game', () => {
   // - Use the functions in util and GameService and alter them iteratively
   //   in order to use the state machine -> also check how to use the actor
 
-  it('creates a new Game', () => {
-    GameService.createGame({ name: nameGame, playerCount: playerCount }, ctx, {
-      roleAssigner,
-    })
+  it('creates a new Game', async () => {
+    if (createNewGame) {
+      game = await GameService.createGame(
+        { name: nameGame, playerCount: playerCount },
+        ctx,
+        { roleAssigner }
+      )
+      gameId = game.id
+    } else {
+      game = await findGame(gameId)
+    }
   })
 })
 
