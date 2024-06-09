e23ac1f9224678df8499a1c972739f7d1459149a
Roland Schläfli <rolandschlaefli@gmail.com>
2024-06-09 09:59:33 +0200

    rework actions reducer with immer

--- apps/demo-game/src/reducers/ActionsReducer.ts
+++ apps/demo-game/src/reducers/ActionsReducer.ts
@@ -1,6 +1,9 @@
 import { Action } from '@gbl-uzh/platform'
 import { debugLog } from '@gbl-uzh/platform/dist/lib/util'
+import { PeriodFacts, PeriodSegmentFacts } from '@graphql/types'
 import { PrismaClient } from '@prisma/client'
+import { produce } from 'immer'
+import { match } from 'ts-pattern'
 
 export enum ActionTypes {
   DECIDE_BANK = 'DECIDE_BANK',
@@ -19,8 +22,22 @@ type PayloadType = {
   playerArgs: {
     decision: boolean
   }
-  segmentFacts: {}
-  periodFacts: {}
+  segmentFacts: PeriodSegmentFacts
+  periodFacts: PeriodFacts
+}
+
+type State = {
+  decisions: {
+    bank: boolean
+    bonds: boolean
+    stocks: boolean
+  }
+}
+
+type ResultState = {
+  type: ActionTypes
+  result: State
+  isDirty: boolean
 }
 
 type Actions =
@@ -28,30 +45,42 @@ type Actions =
   | Action<ActionTypes.DECIDE_BONDS, PayloadType, PrismaClient>
   | Action<ActionTypes.DECIDE_STOCK, PayloadType, PrismaClient>
 
-export function apply(state: any, action: Actions) {
-  const newState = {
+export function apply(state: State, action: Actions): ResultState {
+  // TODO: move this to platform? -> reducer should not have to care about isDirty and other non-user-logicstuff
+  const baseState = {
     type: action.type,
     result: state,
-    isDirty: true,
+    isDirty: false,
   }
 
-  const { decision } = action.payload.playerArgs
+  // TODO: the user reducer could just get the "draft" inside this function as first parameter
+  // TODO: and platform would do all code around it
+  const newState = produce(baseState, (draft) => {
+    const { decision } = action.payload.playerArgs
 
-  switch (action.type) {
-    case ActionTypes.DECIDE_BANK:
-      newState.result.decisions.bank = decision
-      break
-    case ActionTypes.DECIDE_BONDS:
-      newState.result.decisions.bonds = decision
-      break
-    case ActionTypes.DECIDE_STOCK:
-      newState.result.decisions.stocks = decision
-      break
-    default:
-      break
-  }
+    match(action)
+      .with({ type: ActionTypes.DECIDE_BANK }, () => {
+        draft.result.decisions.bank = decision
+      })
+      .with({ type: ActionTypes.DECIDE_BONDS }, () => {
+        draft.result.decisions.bonds = decision
+      })
+      .with({ type: ActionTypes.DECIDE_STOCK }, () => {
+        draft.result.decisions.stocks = decision
+      })
+      .exhaustive()
+  })
 
-  debugLog('ActionsReducer', state, action, newState)
+  // this computes the isDirty flag based on whether there were changes in state from baseState to newState
+  // can try it by commenting the decision changes/match, after which isDirty will log false
+  // TODO: move this to platform code
+  // TODO: -> goal: no isDirty computation anymore inside the reducers, instead just derive it from the state changes (or no changes)
+  // TODO: after this change, we could maybe make sure that input state and output state are the same
+  const resultState = produce(newState, (draft) => {
+    draft.isDirty = baseState !== newState
+  })
 
-  return newState
+  debugLog('ActionsReducer', state, action, newState, resultState)
+
+  return resultState
 }
