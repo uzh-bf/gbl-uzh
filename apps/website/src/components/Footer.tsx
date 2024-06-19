c2e78d6384e5dce8ad8c86fe97b0be85d27f167a
jajakob <j.jacob.cano@gmail.com>
2024-06-19 12:56:47 +0200

    adjusting footer, adding gbl logo

--- apps/website/src/components/Footer.tsx
+++ apps/website/src/components/Footer.tsx
@@ -9,7 +9,7 @@ function Footer() {
   return (
     <footer className="border-top mt-8 bg-slate-100 text-sm text-gray-600 md:text-base">
       <div className="m-auto flex max-w-6xl flex-initial flex-col justify-between px-4 py-8 md:flex-row md:px-8 md:py-16">
-        <div className="order-1 mb-4 flex flex-col items-center gap-2 self-center md:order-2 md:mb-0 md:gap-4">
+        <div className="order-1 mb-4 flex flex-wrap items-center justify-center gap-2 md:order-2 md:mb-0 md:w-1/3 md:justify-end md:gap-4">
           <a href="/" target="_blank" rel="noreferrer">
             <div className="relative h-20 w-40 rounded bg-white hover:outline">
               <Image
@@ -46,13 +46,13 @@ function Footer() {
           </a>
         </div>
 
-        <div className="prose prose-sm order-2 space-y-2 text-center text-gray-600 md:order-1 md:space-y-4 md:text-left">
-          <div className="flex flex-row items-center gap-4">
+        <div className="prose prose-sm order-2 space-y-4 text-gray-600 md:order-1">
+          <div className="flex flex-row gap-4">
             <FontAwesomeIcon
               icon={faBook}
-              className="hidden w-6 text-gray-500 md:block"
+              className="hidden w-6 pt-[0.375rem] text-gray-500 md:block"
             />
-            <div className="flex-1 text-center md:text-left">
+            <div className="flex-1">
               <div className="font-bold">GBL @ UZH</div>
               <div>
                 <a
@@ -67,12 +67,12 @@ function Footer() {
             </div>
           </div>
 
-          <div className="flex flex-row items-center gap-4">
+          <div className="flex flex-row gap-4">
             <FontAwesomeIcon
               icon={faCode}
-              className="hidden w-6 text-gray-500 md:block"
+              className="hidden w-6 pt-[0.375rem] text-gray-500 md:block"
             />
-            <div className="flex-1 text-center md:text-left">
+            <div className="flex-1">
               The GBL Website and Knowledge Base are being developed in public.
               Have a look at our source code on{' '}
               <Link href="https://github.com/uzh-bf/gbl-uzh" target="_blank">
@@ -82,12 +82,12 @@ function Footer() {
             </div>
           </div>
 
-          <div className="flex flex-row items-center gap-4">
+          <div className="flex flex-row gap-4">
             <FontAwesomeIcon
               icon={faMailBulk}
-              className="hidden w-6 text-gray-500 md:block"
+              className="hidden w-6 pt-[0.375rem] text-gray-500 md:block"
             />
-            <div className="flex-1 text-center md:text-left">
+            <div className="flex-1">
               <Link href="/about">Contact Us</Link> or provide feedback on our{' '}
               <Link href="https://gbl-uzh.feedbear.com/roadmap" target="_blank">
                 Public Roadmap
