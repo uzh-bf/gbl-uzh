d3a8259fd703ebc6766ad56fe34a96689de19559
jajakob <j.jacob.cano@gmail.com>
2024-07-02 15:10:25 +0200

    period result reducer typed

--- apps/demo-game/src/reducers/PeriodResultReducer.ts
+++ apps/demo-game/src/reducers/PeriodResultReducer.ts
@@ -6,52 +6,72 @@ import { PlayerRole } from '../settings/Constants'
 
 const INITIAL_CAPITAL = 10000
 
-// TODO(JJ): Double-check if all defined fns have the same input type
-type State = {
+type Assets = {
+  bank: number
+  bonds: number
+  stocks: number
+  totalAssets: number
+}
+
+type StateInit = {
   decisions: {
     bank: boolean
     bonds: boolean
     stocks: boolean
   }
-  assets: {
-    bank: number
-    bonds: number
-    stocks: number
-    totalAssets: number
-  }
+  assets: Assets
 }
 
-// TODO(JJ): Example output
-type OutputState = State & {
-  // additional variables
+type OutputStateInit = {
+  result: StateInit
+  actions?: []
+}
+
+type State = StateInit & {
+  returns: Assets
+  assetsWithReturns: Assets &
+    {
+      ix: number
+      bankReturn?: number
+      bondsReturn?: number
+      stocksReturn?: number
+      totalAssetsReturn?: number
+    }[]
+}
+
+type OutputStateStart = {
+  result: State
+  actions?: []
+}
+
+type OutputStateEnd = OutputStateStart & {
+  events: []
 }
 
 export function initialize(
-  state: State,
+  state: StateInit,
   payload: PayloadPeriodResult<PlayerRole, PeriodFacts>
-) {
-  const baseState = {
+): OutputStateInit {
+  const baseState: OutputStateInit = {
     result: state,
-    isDirty: false,
   }
 
-  const newState = produce(baseState, (draft) => {
-    draft.result.decisions = {
-      bank: true,
-      bonds: false,
-      stocks: false,
+  const resultState: OutputStateInit = produce(
+    baseState,
+    (draft: OutputStateInit) => {
+      draft.result.decisions = {
+        bank: true,
+        bonds: false,
+        stocks: false,
+      }
+      draft.result.assets = {
+        bank: INITIAL_CAPITAL,
+        bonds: 0,
+        stocks: 0,
+        totalAssets: INITIAL_CAPITAL,
+      }
     }
-    draft.result.assets = {
-      bank: INITIAL_CAPITAL,
-      bonds: 0,
-      stocks: 0,
-      totalAssets: INITIAL_CAPITAL,
-    }
-  })
-
-  const resultState = produce(newState, (draft) => {
-    draft.isDirty = baseState !== newState
-  })
+  )
 
   debugLog('PeriodResultInitialize', state, payload, resultState)
   return resultState
@@ -60,15 +80,11 @@ export function initialize(
 export function start(
   state: State,
   payload: PayloadPeriodResult<PlayerRole, PeriodFacts>
-) {
-  const baseState = {
+): OutputStateStart {
+  const baseState: OutputStateStart = {
     result: state,
-    isDirty: false,
   }
-  const newState = produce(baseState, (draft) => {})
-  const resultState = produce(newState, (draft) => {
-    draft.isDirty = baseState !== newState
-  })
+  const resultState = produce(baseState, (draft: OutputStateStart) => {})
 
   debugLog('PeriodResultStart', state, payload, resultState)
   return resultState
@@ -77,17 +93,16 @@ export function start(
 export function end(
   state: State,
   payload: PayloadPeriodResultEnd<PeriodFacts, PeriodSegmentFacts, PlayerRole>
-) {
-  const baseState = {
+): OutputStateEnd {
+  const baseState: OutputStateEnd = {
     result: state,
-    isDirty: false,
+    events: [],
   }
 
-  const newState = produce(baseState, (draft) => {})
-
-  const resultState = produce(newState, (draft) => {
-    draft.isDirty = baseState !== newState
-  })
+  const resultState: OutputStateEnd = produce(
+    baseState,
+    (draft: OutputStateEnd) => {}
+  )
 
   debugLog('PeriodResultEnd', state, payload, resultState)
   return resultState
