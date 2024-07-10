ed8a85536ace8ea263a0bfabfce3a7bfcaadebb8
jajakob <j.jacob.cano@gmail.com>
2024-07-10 13:18:04 +0200

    added logo component

--- packages/ui/src/components/Logo.tsx
+++ packages/ui/src/components/Logo.tsx
@@ -11,10 +11,6 @@ interface Props {
   level?: number
 }
 
-// TODO(JJ): Discuss with RS
-// - path to avatar/locations/ and images in general directly?
-// - double-check: absolute vs relative - something is off
-// - bg color of name is off
 function Logo({ color, avatar, name, location, level }: Props) {
   return (
     <div
@@ -30,18 +26,18 @@ function Logo({ color, avatar, name, location, level }: Props) {
       )}
       {location && (
         <img
-          className="absolute w-10 p-1 bg-white rounded-lg shadow left-2 top-1"
+          className="absolute w-1/4 p-1 bg-white rounded-lg shadow left-2 top-2"
           src={`/locations/${location}.svg`}
         />
       )}
       {name && (
-        <div className="absolute left-0 right-0 p-1 text-center text-black shadow bg-slate-600 bg-opacity-90 bottom-8">
+        <div className="absolute left-0 right-0 p-1 text-center text-white shadow bg-slate-600 bg-opacity-90 bottom-2">
           <div className="text-sm font-semibold">{name}</div>
           <div className="text-xs">HQ: {location}</div>
         </div>
       )}
       {level && (
-        <div className="absolute flex flex-col items-center justify-center w-8 h-8 text-lg font-bold text-black bg-red-500 rounded-full shadow top-0 right-0">
+        <div className="absolute flex flex-col items-center justify-center w-8 h-8 text-lg font-bold text-white bg-red-500 rounded-full shadow top-2 right-2">
           {level}
         </div>
       )}
