36cb2ad2a65da7216181ea33c173c7da18827d15
jajakob <j.jacob.cano@gmail.com>
2024-07-10 13:18:04 +0200

    added logo component

--- apps/demo-game/src/pages/index.tsx
+++ apps/demo-game/src/pages/index.tsx
@@ -1,11 +1,14 @@
-import { Button, Dice } from '@gbl-uzh/ui'
+import { Logo } from '@gbl-uzh/ui'
+import { Progress } from '@uzh-bf/design-system'
 
 export default function Home() {
   return (
     <div className="font-source-sans p-4">
       hello world
-      <Button />
-      <Dice dice1={'dice1'} dice2={'dice2'} />
+      {/* <Button /> */}
+      {/* <Dice dice1={'dice1'} dice2={'dice2'} /> */}
+      <Progress value={50} max={100} formatter={(val) => `${val}XP`} />
+      <Logo name={'Logo'} />
     </div>
   )
 }
