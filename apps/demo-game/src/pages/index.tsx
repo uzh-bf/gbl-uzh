f86ca8996a5aa420080c361f2247f694e50a31d5
jajakob <j.jacob.cano@gmail.com>
2024-07-10 09:06:22 +0200

    Adding dice as ui component

--- apps/demo-game/src/pages/index.tsx
+++ apps/demo-game/src/pages/index.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@gbl-uzh/ui'
+import { Button, Dice } from '@gbl-uzh/ui'
 
 export default function Home() {
   return (
-    <div className="p-4 font-source-sans">
+    <div className="font-source-sans p-4">
       hello world
       <Button />
+      <Dice dice1={'dice1'} dice2={'dice2'} />
     </div>
   )
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,4 @@
 import './globals.css'
 
 export { Button } from '~/components/Button'
+export { Dice } from '~/components/Dice'
