ecbd1b006813b179413f09388572835fa420ff7f
jajakob <j.jacob.cano@gmail.com>
2024-07-10 13:42:14 +0200

    adding player display as ui component

--- apps/demo-game/src/pages/index.tsx
+++ apps/demo-game/src/pages/index.tsx
@@ -1,14 +1,28 @@
-import { Logo } from '@gbl-uzh/ui'
+import { PlayerDisplay } from '@gbl-uzh/ui'
 import { Progress } from '@uzh-bf/design-system'
+import { useRouter } from 'next/router'
 
 export default function Home() {
+  const router = useRouter()
   return (
     <div className="font-source-sans p-4">
       hello world
       {/* <Button /> */}
       {/* <Dice dice1={'dice1'} dice2={'dice2'} /> */}
       <Progress value={50} max={100} formatter={(val) => `${val}XP`} />
-      <Logo name={'Logo'} />
+      <PlayerDisplay
+        name={'playerName'}
+        avatar={''}
+        color={'Red'}
+        achievements={[]}
+        location={'playerFactsLocation'}
+        xp={20}
+        xpToNext={50}
+        level={1}
+        onClick={() => {
+          router.replace('/play/welcome')
+        }}
+      />
     </div>
   )
 }
--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,6 +1,7 @@
 import './globals.css'
 import Logo from '~/components/Logo'
+import PlayerDisplay from '~/components/PlayerDisplay'
 
 export { Button } from '~/components/Button'
-export { Logo }
+export { Logo, PlayerDisplay }
 // export { Dice } from '~/components/Dice'
