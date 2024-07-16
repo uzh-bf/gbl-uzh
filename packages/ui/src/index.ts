5d74f8f150b0c4ff2a4bb69b40340ef4a1f003b1
jajakob <j.jacob.cano@gmail.com>
2024-07-16 17:01:39 +0200

    timeline entry

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -3,7 +3,8 @@ import Logo from '~/components/Logo'
 import PlayerDisplay from '~/components/PlayerDisplay'
 import StorageOverview from '~/components/StorageOverview'
 import ProbabilityChart from '~/components/ProbabilityChart'
+import TimelineEntry from '~/components/TimelineEntry'
 
 export { Button } from '~/components/Button'
-export { Logo, PlayerDisplay, StorageOverview, ProbabilityChart }
+export { Logo, PlayerDisplay, StorageOverview, ProbabilityChart, TimelineEntry }
 // export { Dice } from '~/components/Dice'
