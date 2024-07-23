170db34f0a3079015ba3e7949aa3315eecf3258a
jajakob <j.jacob.cano@gmail.com>
2024-07-23 08:21:56 +0200

    demo-game: initial modification of welcome page

--- apps/demo-game/src/pages/play/welcome.tsx
+++ apps/demo-game/src/pages/play/welcome.tsx
@@ -1,4 +1,5 @@
 import { useMutation } from '@apollo/client'
+import { Logo } from '@gbl-uzh/ui'
 import { Button, FormikTextField } from '@uzh-bf/design-system'
 import { Form, Formik } from 'formik'
 import { useRouter } from 'next/router'
@@ -12,6 +13,12 @@ const Schema = Yup.object().shape({
     .required('Required'),
 })
 
+// TODO(JJ):
+// - Maybe move to ui package
+// - Double-check if for this game location is needed?
+// - LogoSelector
+// - Will all games have the same greeting layout?
+// - Maybe change title (now: Demo Game)
 function Welcome() {
   const router = useRouter()
 
@@ -22,6 +29,13 @@ function Welcome() {
       <Formik
         initialValues={{
           name: 'Your Name',
+          color: 'red',
+          location: 'ZH',
+          avatar: 'avatar_placeholder',
+          // color: player.color,
+          // avatar: AVATARS[player.role][0]['key'],
+          // name: player.name,
+          // location: LOCATIONS[player.role][0],
         }}
         onSubmit={async (values) => {
           console.log(values)
@@ -34,12 +48,58 @@ function Welcome() {
         }}
       >
         {({ values, errors, touched }) => (
-          <Form className="">
-            <FormikTextField label="Trader Name" name="name" />
-            <Button className={{ root: 'mt-8' }} type="submit">
-              Start Game
-            </Button>
-          </Form>
+          <div className="flex flex-col gap-4">
+            <div className="text-xl font-bold">Welcome to the Demo Game!</div>
+            <div className="flex flex-row gap-8">
+              <div className="w-48 flex-initial">
+                <Logo
+                  color={values.color}
+                  location={values.location}
+                  name={values.name}
+                  avatar={values.avatar}
+                />
+              </div>
+              <div className="prose flex-1">
+                Welcome, {values.name}! Your company has its headquarters in{' '}
+                {values.location}.
+              </div>
+            </div>
+
+            <Form className="">
+              <div className="flex flex-row gap-8">
+                {/* <LogoSelector
+                  className="w-48 flex-none"
+                  color={values.color}
+                  name="avatar"
+                /> */}
+
+                <div className="flex-initial space-y-3">
+                  <FormikTextField label="Trader Name" name="name" />
+                  {/* <FormikSelectField
+                    label="Location"
+                    name="location"
+                    items={LOCATIONS[player.role].map((key) => ({
+                      value: key,
+                      label: key,
+                    }))}
+                  />
+                  <FormikSelectField
+                    label="Color"
+                    name="color"
+                    items={Object.keys(COLORS).map((label) => ({
+                      value: label,
+                      label,
+                    }))}
+                  /> */}
+                </div>
+              </div>
+
+              {/* <FormikTextField label="Trader Name" name="name" /> */}
+              <Button className={{ root: 'mt-8' }} type="submit">
+                Start Game
+              </Button>
+            </Form>
+          </div>
         )}
       </Formik>
     </div>
