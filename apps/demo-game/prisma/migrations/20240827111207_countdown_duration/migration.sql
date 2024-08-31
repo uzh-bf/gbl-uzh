cd4d676a969bd56b1eebdb473b623259a8b94747
jajakob <j.jacob.cano@gmail.com>
2024-08-31 16:27:05 +0200

    fixing migrations countdown

--- apps/demo-game/prisma/migrations/20240827111207_countdown_duration/migration.sql
+++ apps/demo-game/prisma/migrations/20240827111207_countdown_duration/migration.sql
@@ -1,18 +1,2 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `avatar` on the `Player` table. All the data in the column will be lost.
-  - You are about to drop the column `color` on the `Player` table. All the data in the column will be lost.
-  - You are about to drop the column `location` on the `Player` table. All the data in the column will be lost.
-
-*/
 -- AlterTable
-ALTER TABLE "PeriodSegment" ADD COLUMN     "countdownDurationMs" INTEGER,
-ADD COLUMN     "countdownExpiresAt" TIMESTAMP(3);
-
--- AlterTable
-ALTER TABLE "Player" DROP COLUMN "avatar",
-DROP COLUMN "color",
-DROP COLUMN "location",
-ADD COLUMN     "number" INTEGER NOT NULL DEFAULT -1,
-ALTER COLUMN "name" SET DEFAULT 'Anonymous';
+ALTER TABLE "PeriodSegment" ADD COLUMN     "countdownDurationMs" INTEGER;
