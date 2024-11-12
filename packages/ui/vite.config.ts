06b5a8667a70b65e2eb53b73629a922925b903dd
Roland Schläfli <rolandschlaefli@gmail.com>
2024-11-12 21:08:14 +0100

    external deps in ui package

--- packages/ui/vite.config.ts
+++ packages/ui/vite.config.ts
@@ -2,10 +2,18 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { externalizeDeps } from 'vite-plugin-externalize-deps'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    dts({ rollupTypes: true }),
+    externalizeDeps({
+      deps: false,
+      peerDeps: true,
+    }) as Plugin,
+  ],
   build: {
     lib: {
       entry: path.resolve(__dirname, 'src/index.ts'),
@@ -13,7 +21,9 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      watch: {
+        include: 'src/**',
+      },
     },
   },
   resolve: {
