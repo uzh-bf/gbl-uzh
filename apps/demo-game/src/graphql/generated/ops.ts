7d70d4d8ca6201f4ac742ccc0aec05a4f7e4e164
Roland Schläfli <rolandschlaefli@gmail.com>
2024-11-13 10:08:00 +0100

    gql codegen updates

--- apps/demo-game/src/graphql/generated/ops.ts
+++ apps/demo-game/src/graphql/generated/ops.ts
@@ -1,5 +1,5 @@
-import { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
-import { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
+import type { GraphQLResolveInfo, GraphQLScalarType, GraphQLScalarTypeConfig } from 'graphql';
+import type { TypedDocumentNode as DocumentNode } from '@graphql-typed-document-node/core';
 export type Maybe<T> = T | null;
 export type InputMaybe<T> = Maybe<T>;
 export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
