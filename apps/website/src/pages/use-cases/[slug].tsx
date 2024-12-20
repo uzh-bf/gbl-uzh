09537613f72cedd1f94160b7aae1ea9d2b945cce
Roland Schläfli <rolandschlaefli@gmail.com>
2024-12-20 17:32:04 +0100

    chore: improve UC sidebar

--- apps/website/src/pages/use-cases/[slug].tsx
+++ apps/website/src/pages/use-cases/[slug].tsx
@@ -32,40 +32,56 @@ function UseCase({
         <PageWithHeader title={source.frontmatter.title}>
           <TitleBackground>
             <H1 className={{ root: 'mx-auto max-w-6xl md:pl-4' }}>
-              {source.frontmatter.title}
+              Use Cases
             </H1>
           </TitleBackground>
 
           <Content>
             <div className="flex flex-row gap-8">
               <div className="hidden w-80 flex-initial rounded border bg-slate-50 p-4 md:block">
-                <H2>Use Cases</H2>
-                <ul className="text-sm">
-                  {sourceArr[0].map(({ frontmatter }: any) => (
-                    <Link
-                      className="block border-b py-1 last:border-b-0"
-                      key={frontmatter.slug}
-                      href={`/use-cases/${frontmatter.slug}`}
-                    >
-                      <li
-                        className={twMerge(
-                          'hover:cursor-pointer hover:text-orange-600',
-                          router.query.slug === frontmatter.slug && 'font-bold'
-                        )}
-                      >
-                        {frontmatter.title}
-                      </li>
-                    </Link>
-                  ))}
-                </ul>
+                {['didactics', 'development', 'simulations'].map((type) => {
+                  const useCases = sourceArr[0].filter(
+                    ({ frontmatter }: any) => frontmatter.type === type
+                  )
+                  if (useCases.length === 0) return null
+
+                  return (
+                    <div key={type} className="mb-4 last:mb-0">
+                      <h3 className="mb-1 font-bold capitalize">
+                        {type === 'development' ? 'Game Development' : type}
+                      </h3>
+                      <ul className="text-sm">
+                        {useCases.map(({ frontmatter }: any) => (
+                          <Link
+                            className="block border-b py-1 last:border-b-0"
+                            key={frontmatter.slug}
+                            href={`/use-cases/${frontmatter.slug}`}
+                          >
+                            <li
+                              className={twMerge(
+                                'hover:cursor-pointer hover:text-orange-600',
+                                router.query.slug === frontmatter.slug && 'font-bold'
+                              )}
+                            >
+                              {frontmatter.title}
+                            </li>
+                          </Link>
+                        ))}
+                      </ul>
+                    </div>
+                  )
+                })}
               </div>
-              <Prose
-                className={{
-                  root: 'max-w-none flex-1 prose-headings:mb-[0.5em] prose-headings:font-sans prose-h2:text-xl',
-                }}
-              >
-                <MDXRemote {...source} components={components} />
-              </Prose>
+              <div className="flex-1">
+                <H1 className={{ root: 'mb-8' }}>{source.frontmatter.title}</H1>
+                <Prose
+                  className={{
+                    root: 'max-w-none prose-headings:mb-[0.5em] prose-headings:font-sans prose-h2:text-xl',
+                  }}
+                >
+                  <MDXRemote {...source} components={components} />
+                </Prose>
+              </div>
             </div>
           </Content>
         </PageWithHeader>
