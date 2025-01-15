f8fe964a1ebf1af630199cdefb0c7fb5273df1e0
jajakob <j.jacob.cano@gmail.com>
2025-01-15 13:22:46 +0100

    try to do it with apollo subscription

--- apps/demo-game/src/pages/_app.tsx
+++ apps/demo-game/src/pages/_app.tsx
@@ -1,10 +1,11 @@
-import { ApolloProvider } from '@apollo/client'
+import { ApolloProvider, useSubscription } from '@apollo/client'
 import { config } from '@fortawesome/fontawesome-svg-core'
 import { useApollo } from '@gbl-uzh/platform/dist/lib/apollo'
 import { SessionProvider } from 'next-auth/react'
 import type { AppProps } from 'next/app'
 import { Toaster } from '../components/ui/toaster'
 // import { Toaster } from '@uzh-bf/design-system'
+import { GlobalEventsDocument } from 'src/graphql/generated/ops'
 
 import '@fortawesome/fontawesome-svg-core/styles.css'
 import RootLayout from '../components/RootLayout'
@@ -17,6 +18,17 @@ export default function App({
   pageProps: { session, ...pageProps },
 }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState)
+
+  useSubscription(GlobalEventsDocument, {
+    shouldResubscribe: true,
+    onData: ({ data }) => {
+      // const events = data?.data?.eventsGlobal
+      // if (events?.length > 0) {
+      //   events.forEach((event) => toast(event.type))
+      // }
+    },
+    client: apolloClient,
+  })
 
   return (
     <RootLayout>
