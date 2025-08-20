40df731293306112ec53277d43c144284425deba
jajakob <j.jacob.cano@gmail.com>
2025-08-20 10:05:55 +0200

    remove old retry fn import

--- packages/platform/src/services/PlayService.ts
+++ packages/platform/src/services/PlayService.ts
@@ -9,7 +9,6 @@ import {
 import * as EventService from './EventService.js'
 import dayjs from 'dayjs'
 import log from '../lib/logger.js'
-import { withRetry } from 'src/lib/util.js'
 
 type Context = CtxWithPrisma<DB.PrismaClient>
 
